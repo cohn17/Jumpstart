a5fdaca51e94275ad1cd164e8d2db04b4754eb85
Paul Cohn <paulcohn@Pauls-MacBook-Pro.local>
2018-06-03 20:27:44 +0700

    main.js not working.

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,4 +9,13 @@ $(document).ready(function() {
     slidesToShow: 1,
     speed: 4000
   });
+  $(window).scroll(function() {
+    if ($(document).scrollTop > 50) {
+      $('.navbar').css({top:"-50px"});
+    }
+    else {
+      $('.navbar').css({top:"0px"});
+    }
+  });
+
 });
